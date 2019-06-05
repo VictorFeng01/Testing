8ea4e6bad59fb3ab9b2db0f772ac8786eec88d9b
chockie_zhu <winnie.j.sun>
2019-06-05 10:45:00 +0800

    commit code for test

---
+++ Victor/src/test/java/ForJenkins.java
@@ -0,0 +1,8 @@
+/**
+ * Created by mobiletestingdevice on 2019/6/5.
+ */
+public class ForJenkins {
+    public static void  main(String []args){
+        System.out.print("Hello World!");
+    }
+}
