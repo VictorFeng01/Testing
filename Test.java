ef5641af07a60e0149d317b5b7cfbb904b18d9a2
chockie_zhu <winnie.j.sun>
2019-06-05 10:35:27 +0800

    delete

--- Test.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by mobiletestingdevice on 2019/6/5.
- */
-public class Test {
-    public static void main(String[] args){
-        String str = "Hello World!";
-        System.out.print(str);
-    }
-}
