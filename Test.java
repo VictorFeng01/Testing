6c2d720a5e4158ccb4bd668c8456cbf1171f8904
VictorFeng01 <51391026+VictorFeng01@users.noreply.github.com>
2019-06-05 10:25:09 +0800

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,9 @@
+/**
+ * Created by mobiletestingdevice on 2019/6/5.
+ */
+public class Test {
+    public static void main(String[] args){
+        String str = "Hello World!";
+        System.out.print(str);
+    }
+}
